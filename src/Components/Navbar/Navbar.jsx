757732114fc8fca7b4dfdf0d054c67811c19b628
Enduxion <endux.pg@gmail.com>
2025-01-26 00:06:51 +0545

    Incomplete!, setting logo image

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,42 @@
 import { GroupyIcon } from "../../assets/image";
 
 const Navbar = () => {
-    const currentUserId = "1";
-    const navElements = [
-        {
-            name: "Friends",
-            path: "/friends"
-        },
-        {
-            name: "Profile",
-            path: `/profile/${currentUserId}`
-        },
-        {
-            name: "Settings",
-            path: "/settings"
-        }
-    ]
-    return (
-        <div className="">
-            <img src={GroupyIcon} className="" />
-            <div className="">
-                {navElements.map((element) => {
-                    return (
-                        <a href={element.path} className="">{element.name}</a>
-                    )
-                })}
-            </div>
-        </div>
-    );
+  const currentUserId = "1";
+  const navElements = [
+    {
+      name: "Friends",
+      path: "/friends",
+    },
+    {
+      name: "Profile",
+      path: `/profile/${currentUserId}`,
+    },
+    {
+      name: "Settings",
+      path: "/settings",
+    },
+  ];
+  return (
+    <div className="flex flex-row h-nav w-full justify-between item-center">
+      <img
+        src={GroupyIcon}
+        alt="Groupy Icon"
+        style={{
+          height: "40px",
+          width: "40px",
+        }}
+      />
+      <div className="flex">
+        {navElements.map((element) => {
+          return (
+            <a href={element.path} className="">
+              {element.name}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/assets/image.js
+++ src/assets/image.js
@@ -1,3 +1,3 @@
-import GroupyIcon from "./GroupyIcon.png";
+import GroupyIcon from "./images/GroupyIcon.png";
 
 export { GroupyIcon };
